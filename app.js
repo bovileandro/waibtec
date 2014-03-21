a0de03389b093506ef911c880e1e13c0dbb271cf
Thiago Porto <tporto88@gmail.com>
2014-03-21 20:18:53 -0300

    Teste

--- app.js
+++ app.js
@@ -3,12 +3,19 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var load    = require('express-load');
-var http    = require('http');
-var path    = require('path');
+var express  = require('express');
+var load     = require('express-load');
+var http     = require('http');
+var path     = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/waibtec', function(err){
+  if (err){
+    console.log('Erro ao conectar no mongodb: '+err);
+  }
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -28,36 +35,6 @@ if ('development' == app.get('env')) {
 }
 
 load('models').then('controllers').then('routes').into(app);
-
-
-
-
-/*MongoDB*/
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/waibtec');
-
-var db = mongoose.connect;
-
-var kittySchema = mongoose.Schema({
-    name: String
-});
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-var silence = new Kitten({ name: 'Silence' })
-
-silence.save(function (err, fluffy) {
-  if (err) return console.error(err);
-  console.log('salvo com sucesso');
-});
-
-
-//db.on('error', console.error.bind(console, 'connection error:'));
-
-//db.once('open', function callback () {
-  //console.log('Banco de dados rodando...');
-//});
-
-
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,11 +1,10 @@
 module.exports = function(app){
 
+	var Usuario = app.models.usuarios;
+
 	var UsuarioController = {
 		index: function(req,res){
 			res.render("usuarios/index");
-		},
-		cadastro: function(req,res){
-			//
 		}
 	}
 
